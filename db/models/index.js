32451bde8a8ba9e646ba44e85c2d1f39a1956734
Totala <Shree.Totala@Dell.com>
2022-03-06 12:56:31 +0530

    Envrionment Variables added

--- db/models/index.js
+++ db/models/index.js
@@ -6,12 +6,28 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const config = require('../config/config');
 //postgres://vdmtigqqlbkhca:1248460f600c4af10ac580606f71ba2702df9c15f254727a9cb66679442094b2@ec2-54-83-21-198.compute-1.amazonaws.com:5432/de3nlpe6p2230f
+// const sequelize = new Sequelize({
+//   database: "de3nlpe6p2230f",
+//   username: "vdmtigqqlbkhca",
+//   password: "1248460f600c4af10ac580606f71ba2702df9c15f254727a9cb66679442094b2",
+//   host: "ec2-54-83-21-198.compute-1.amazonaws.com",
+//   port: 5432,
+//   dialect: "postgres",
+//   dialectOptions: {
+//     ssl: {
+//       require: true,
+//       rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+//     }
+//   }
+// ,"define": { freezeTableName: true}
+// });
+
 const sequelize = new Sequelize({
-  database: "de3nlpe6p2230f",
-  username: "vdmtigqqlbkhca",
-  password: "1248460f600c4af10ac580606f71ba2702df9c15f254727a9cb66679442094b2",
-  host: "ec2-54-83-21-198.compute-1.amazonaws.com",
-  port: 5432,
+  database: process.env.POSTGRES_DB,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
   dialect: "postgres",
   dialectOptions: {
     ssl: {
